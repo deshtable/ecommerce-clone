8ea43619a5ce957067a45b576d910776c79e9abf
Desh Padmakumar <deshspo@gmail.com>
2020-12-09 15:49:18 -0800

    preliminary node emailer works yayayayayay!!!!!

--- email.js
+++ email.js
@@ -4,13 +4,13 @@ const nodemailer = require('nodemailer');
 async function sendemail(){
 
     let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
+        service: 'gmail',
         port: 587,
         secure: false,
-    auth: {
-        user: 'deshstephen@gmail.com',
-        pass: 'desh.stephen01'
-    }
+        auth: {
+            user: 'deshstephen@gmail.com',
+            pass: 'desh.stephen01'
+        }
     });
 
     let emailInfo = await transporter.sendMail({
@@ -20,12 +20,12 @@ async function sendemail(){
         text: 'That was easy!'
         });
 
-        let info = transporter.sendMail(emailInfo, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
+    let info = transporter.sendMail(emailInfo, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
         });
 }
 
